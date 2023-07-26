039f4ab4b47259f8dbc512d986bf567a834505d7
lizzyraesetja <lizzyraesetja2@gmail.com>
2023-07-26 13:09:14 +0000

    2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
+"""
+place holder
+"""
 
-import json
-import requests as r
-import sys
 
-if __name__ == '__main__':
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    usr = r.get(url + "users/{}".format(user_id)).json()
-    username = usr.get("username")
-    to_do = r.get(url + "todos", params={"user_id": user_id}).json()
+if __name__ == "__main__":
 
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{"task": e.get("title"),
-                              "completed": e.get("completed"),
-                              "username": username} for e in to_do]},
-                  jsonfile)
+    import requests
+    from sys import argv
+    import json
+
+    if len(argv) < 2:
+        exit()
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}"
+        .format(argv[1]))
+    name = requests.get(
+        "https://jsonplaceholder.typicode.com/users?id={}".format(argv[1]))
+    name = name.json()
+    name = name[0]["username"]
+    todos = todos.json()
+    result = {}
+    result[argv[1]] = []
+    for todo in todos:
+        result[argv[1]].append(
+            {"task": todo["title"], "completed": todo["completed"],
+             "username": name})
+    with open("{}.json".format(argv[1]), 'w') as result_file:
+        json.dump(result, result_file)
